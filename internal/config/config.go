292a7bde8693ac2ca4dbc229f1ab9d6329df0d8d
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2026-08-29 23:20:25 +0330

    🎉 config of go added

---
+++ internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type config struct {
+	Port int
+	Env string
+}
+
+func load() (*config, error) {
+	port , err := getEnvInt("PORT" , 8080)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config{
+		Port: port,
+		Env: getEnvString("ENV", "dev"),
+	}, nil
+}
+
+func getEnvString(key, fallback string) string{
+	val, ok := os.LookupEnv(key)
+	if !ok || val == "" {
+		return fallback
+	}
+	return val
+}
+
+func getEnvInt(key string, fallback int) (int, error){
+	val , ok := os.LookupEnv(key)
+	if !ok || val == "" {
+		return fallback, nil
+	}
+	n, err := strconv.Atoi(val)
+	if err != nil {
+		return 0, fmt.Errorf("env var %s: %q is not a valid integer", key, val)
+	}
+	return n, nil
+}
